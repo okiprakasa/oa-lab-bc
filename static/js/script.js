c8fdab6ba61769cf9a1fd71c85934ab3e75004a4
Oki Prakasa <okiprakasa@gmail.com>
2022-04-22 22:07:36 +0800

    - Add toggle day/night effect to log in - Minimize sidebar width - Delete search listener on click

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,9 @@
 const body = document.querySelector('body'),
       sidebar = body.querySelector('nav'),
       toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
       modeSwitch = body.querySelector(".toggle-switch"),
       searchText = document.getElementById("search-text"),
       modeText = body.querySelector(".mode-text");
-
 toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
     if (searchText.placeholder === "") {
@@ -14,19 +12,8 @@ toggle.addEventListener("click" , () =>{
         searchText.placeholder = "";
     }
 })
-
-searchBtn.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-    if (searchText.placeholder === "") {
-        searchText.placeholder.toggle("Search...");
-    } else {
-        searchText.placeholder.toggle("");
-    }
-})
-
 modeSwitch.addEventListener("click" , () =>{
     body.classList.toggle("dark");
-    
     if(body.classList.contains("dark")){
         modeText.innerText = "Night";
     }else{
